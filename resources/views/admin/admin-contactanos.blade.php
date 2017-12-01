356e4046d473ef28fc4420c7a9fb3f54e098034f
edwin buitrago <edwinbuitrago@qdata.io>
2017-12-01 14:58:15 -0500

    edit contactanos

--- resources/views/admin/admin-contactanos.blade.php
+++ resources/views/admin/admin-contactanos.blade.php
@@ -1,25 +1,5 @@
 @include('admin.admin-header')
 
-                <div id="main-container">
-                    <header class="navbar navbar-inverse navbar-fixed-top">
-                        <ul class="nav navbar-nav-custom">
-                            <li>
-                                <a href="javascript:void(0)" onclick="App.sidebar('toggle-sidebar');this.blur();">
-                                    <i class="fa fa-ellipsis-v fa-fw animation-fadeInRight" id="sidebar-toggle-mini"></i>
-                                    <i class="fa fa-bars fa-fw animation-fadeInRight" id="sidebar-toggle-full"></i>
-                                </a>
-                            </li>
-                        </ul>
-
-                        <ul class="nav navbar-nav-custom pull-right">
-                            <li class="dropdown">
-                                <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" style="padding-right: 20px">
-                                    Cerrar Sesión
-                                </a>
-                            </li>
-                        </ul>
-                    </header>
-
                     <div id="page-content">
                         <div class="block full">
                             <div class="block-title">
